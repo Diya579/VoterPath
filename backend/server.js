85ab17424007cb80273e09f8aaf8a9d89a7746c8
Diya579 <diyas579@gmail.com>
2026-05-03 23:27:14 +0530

    fix: use terminal middleware for express 5 catchall

--- backend/server.js
+++ backend/server.js
@@ -105,7 +105,7 @@ const globalErrorHandler = (err, req, res, next) => {
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('(.*)', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
